31e7adf56f90e54803f911fbf7c5d015004e5a8d
kasinatg <kasinatg@mail.gvsu.edu>
2016-04-01 04:23:17 +0000

    initial commit

--- Gemfile
+++ Gemfile
@@ -15,6 +15,10 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
+
+gem 'responders'
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -30,6 +34,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+ruby '2.3.0' 
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,11 +1,17 @@
 class BugsController < ApplicationController
   before_action :set_bug, only: [:show, :edit, :update, :destroy]
+  before_action :all_bugs, only:[:index, :create, :destroy, :update]
   before_action :set_statuses, :set_issuetypes, :set_priorities
+  
+  respond_to :html, :js
+  
+  def all_bugs
+     @bugs = Bug.all
+  end
 
   # GET /bugs
   # GET /bugs.json
   def index
-    @bugs = Bug.all
   end
 
   # GET /bugs/1
@@ -21,48 +27,64 @@ class BugsController < ApplicationController
   # GET /bugs/1/edit
   def edit
   end
+  
+  def create
+    @bug = Bug.new(bug_params)
+    user = User.find(bug_params[:user_id])
+    @bug.build_user(:id => user.id)
+    @bug.save!
+    @current_bug = @bug
+  end 
 
   # POST /bugs
   # POST /bugs.json
-  def create
-    @bug = Bug.new(bug_params)
+  # def create
+  #   @bug = Bug.new(bug_params)
 
-    respond_to do |format|
-      if @bug.save
-        format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
-        format.json { render :show, status: :created, location: @bug }
+  #   respond_to do |format|
+  #     if @bug.save
+  #       format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
+  #       format.json { render :show, status: :created, location: @bug }
         
-        user = User.find(bug_params[:user_id])
-        @bug.build_user(:id => user.id)
-      else
-        format.html { render :new }
-        format.json { render json: @bug.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #       user = User.find(bug_params[:user_id])
+  #       @bug.build_user(:id => user.id)
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @bug.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /bugs/1
   # PATCH/PUT /bugs/1.json
-  def update
-    respond_to do |format|
-      if @bug.update(bug_params)
-        format.html { redirect_to @bug, notice: 'Bug was successfully updated.' }
-        format.json { render :show, status: :ok, location: @bug }
-      else
-        format.html { render :edit }
-        format.json { render json: @bug.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @bug.update(bug_params)
+  #       format.html { redirect_to @bug, notice: 'Bug was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @bug }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @bug.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /bugs/1
   # DELETE /bugs/1.json
+  # def destroy
+  #   @bug.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to bugs_url, notice: 'Bug was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+  
+  def update
+     @bug.update!(bug_params)
+  end
+  
   def destroy
-    @bug.destroy
-    respond_to do |format|
-      format.html { redirect_to bugs_url, notice: 'Bug was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @bug.destroy!
   end
   
    def set_statuses
